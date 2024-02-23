510005a389c47ba4d77cd8e561eb5744c0dd67a3
jamal daajna <djamal84@gmail.com>
2024-02-23 16:39:22 +0100

    rails watch list

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
-  validates :title, :overview, presence: true
-  validates :title, uniqueness: true
   has_many :bookmarks
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
 end
